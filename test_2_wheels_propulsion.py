8f95c1b469f9a3dcd2d60c28e72589625f348e35
David Hilbert <a.giavaras@gmail.com>
2019-08-30 22:03:09 +0300

    change implementation in propulsion

--- test_2_wheels_propulsion.py
+++ test_2_wheels_propulsion.py
@@ -13,7 +13,7 @@ def test_move_fwd():
     prop = Propulsion2Motors(in_pin_1_motor_1 = IN_PIN_1_MOTOR_1,
                              in_pin_2_motor_1 = IN_PIN_2_MOTOR_1,
                              en_pin_motor_1=ENA_MOTOR_1_PIN_ID,
-                             in_pin_1_motor_2 = None, in_pin_2_motor_2=None)
+                             in_pin_1_motor_2 = None, in_pin_2_motor_2=None, en_pin_motor_2=None)
 
     p = GPIO.PWM(ENA_MOTOR_1_PIN_ID, 1000)
     p.start(100)
@@ -28,7 +28,7 @@ def test_move_bwd():
     prop = Propulsion2Motors(in_pin_1_motor_1 = IN_PIN_1_MOTOR_1,
                              in_pin_2_motor_1 = IN_PIN_2_MOTOR_1,
                              en_pin_motor_1 = ENA_MOTOR_1_PIN_ID,
-                             in_pin_1_motor_2 = None, in_pin_2_motor_2=None)
+                             in_pin_1_motor_2 = None, in_pin_2_motor_2=None, en_pin_motor_2=None)
     p = GPIO.PWM(ENA_MOTOR_1_PIN_ID, 1000)
     p.start(100)
     prop.backward(10)
@@ -38,15 +38,17 @@ def test_move_bwd():
 
 if __name__ == '__main__':
 
-    #try:
+    try:
 
-    GPIO.setmode(GPIO.BCM)
+        GPIO.setmode(GPIO.BCM)
+        test_move_fwd()
+        GPIO.cleanup()
 
-    test_move_fwd()
-    GPIO.cleanup()
+        GPIO.setmode(GPIO.BCM)
+        test_move_bwd()
+        GPIO.cleanup()
 
-    GPIO.setmode(GPIO.BCM)
-    test_move_bwd()
-    GPIO.cleanup()
-    #except:
-    #    GPIO.cleanup()
+    except Exception as e:
+        print("An exception occured whilst runnning the test..." + str(e))
+    finally:
+        GPIO.cleanup()
