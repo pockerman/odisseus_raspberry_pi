44f7307737ef315b55478b0dfb5a3050e141d80c
David Hilbert <a.giavaras@gmail.com>
2019-09-21 15:59:40 +0300

    #31 Add Ultrasound sensor class and tests

--- test_ultrasound.py
+++ test_ultrasound.py
@@ -1,10 +1,80 @@
+
+import time
+from multiprocessing import Process
+
+from ultrasound_sensor import UltrasoundSensor
+from ultrasound_sensor import UltrasoundSensorPort
+from ultrasound_sensor import UltrasoundSensorMsg
+
+
+def distance_calculator(Gpio, ECHO_PIN):
+
+    """
+    dummy distance calculator
+    """
+    return 0.0
+
+
+def test_setup(odisseus_configuration):
+    """
+    Test the setup method
+    """
+
+    print("\t test_setup")
+
+    port_inst = UltrasoundSensorPort(max_size = odisseus_configuration.ULTRASOUND_PORT_MAX_SIZE)
+
+    # this will trigger the setup
+    sensor = UltrasoundSensor(odisseus_config=odisseus_configuration, port_inst=port_inst)
+    assert sensor.is_setup(), "Sensor is not setup properly"
+
+
+def test_assign_msg(odisseus_configuration):
+
+    """
+    Test assign message
+    """
+
+    print("\t test_assign_msg")
+
+    port_inst = UltrasoundSensorPort(max_size = odisseus_configuration.ULTRASOUND_PORT_MAX_SIZE)
+    sensor = UltrasoundSensor(odisseus_config=odisseus_configuration, port_inst=port_inst, distance_calculator=distance_calculator)
+
+    assert sensor.is_setup(), "Sensor is not setup properly"
+
+    kwargs = dict()
+    kwargs['ULTRA_SOUND_TRIGGER_PULSE_TIME'] = odisseus_configuration.ULTRA_SOUND_TRIGGER_PULSE_TIME
+
+    sensor_process = Process(target=sensor.run, kwargs=kwargs)
+    sensor_process.start()
+    sensor_process.join()
+
+    # terminate the sensor process
+    sensor_process.terminate()
+
+    assert port_inst.size() == 1, "No distance message was set"
+
+
 def test(odisseus_configuration):
 
     try:
 
         print("============================")
-        print("Executing Ultrasound Tests")
-        print("Done Executing Ultrasound Tests")
+        print("Executing Ultrasound Tests...")
+
+        if odisseus_configuration.ON_RASP_PI:
+            import RPi.GPIO as GPIO
+        else:
+            from gpio_mock import GPIOMock as GPIO
+
+        # need to set the board mode before doing anything with the pins
+        GPIO.setmode(GPIO.BCM)
+
+        test_setup(odisseus_configuration=odisseus_configuration)
+        test_assign_msg(odisseus_configuration=odisseus_configuration)
+
+        print("Done Executing Ultrasound Tests...")
+        print("============================")
         print("============================")
 
     except Exception as e:
