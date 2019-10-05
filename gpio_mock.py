1c3b77aaaffbd6c9a628cdf42c1bab12dac9750f
David Hilbert <a.giavaras@gmail.com>
2019-10-05 21:18:05 +0300

    #43 Conform to PEP8

--- gpio_mock.py
+++ gpio_mock.py
@@ -7,13 +7,14 @@ class Pwd:
     def start(self, speed):
         pass
 
+
 class GPIOMock:
 
     HIGH = 1
-    LOW  = 0
-    OUT  = 2
-    BCM  = 3
-    IN   = 4
+    LOW = 0
+    OUT = 2
+    BCM = 3
+    IN = 4
 
     @staticmethod
     def setup(pin_id, mode):
