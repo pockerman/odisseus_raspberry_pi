6c3fb911d93751f1d1b8e085e78063801b60f5c0
David Hilbert <a.giavaras@gmail.com>
2019-10-06 22:11:25 +0300

    #43 Bug fixes

--- master_process.py
+++ master_process.py
@@ -24,18 +24,20 @@ class MasterProcess:
     def get_processes_names(self):
         return self._processes.keys()
 
-    def create_processes(self):
+    def create_processes(self, **kwargs):
+
         """
         Create the processes needed for Odisseus
         """
 
         if self._config.ENABLE_MOTORS is True:
             # launch the motor process
-            self._create_motors_process()
+            self._create_motors_process(**kwargs)
 
         if self._config.ENABLE_ULTRASOUND_SENSOR is True:
             # launch the ultrasound sensor process
-            self._create_ultrasound_process()
+            self._create_ultrasound_process(**kwargs)
+
         self._processes_created = True
 
     def terminate_all_processes(self):
@@ -44,14 +46,14 @@ class MasterProcess:
             for proc_name in self._processes:
                 self._processes[proc_name].stop()
 
-    def run(self):
+    def run(self, **kwargs):
 
         """
         Start running the master process
         """
 
         if self._processes_created is False:
-            self.create_processes()
+            self.create_processes(**kwargs)
 
         while True:
 
@@ -61,20 +63,19 @@ class MasterProcess:
             # poll the sensors to get information about the world state
             print("Running master process")
 
-
-    def _create_motors_process(self):
+    def _create_motors_process(self, **kwargs):
 
         """
         Create the motor process
         """
 
-        self._processes.update({"PropulsionControlProcess": PropulsionProcess(odisseus_config=self._config)})
-        self._processes["PropulsionControlProcess"].start(propulsion=None)
+        self._processes.update({PropulsionProcess.process_name(): PropulsionProcess(odisseus_config=self._config)})
+        self._processes[PropulsionProcess.process_name()].start(**kwargs)
 
-    def _create_ultrasound_process(self):
+    def _create_ultrasound_process(self, **kwargs):
 
         self._processes.update({UltrasoundSensorProcess.process_name(): UltrasoundSensorProcess(odisseus_config=self._config,
                                                                                                 port_max_size=self._config.ULTRASOUND_SENSOR_PORT_MAX_SIZE,
                                                                                                 distance_calculator=None)})
-        self._processes[UltrasoundSensorProcess.process_name()].start()
+        self._processes[UltrasoundSensorProcess.process_name()].start(**kwargs)
 
