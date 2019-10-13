44f7dfddd0f2c07658621176db9c8bf8294dbce7
David Hilbert <a.giavaras@gmail.com>
2019-10-13 22:54:16 +0300

    #43 Add WebAppProcess class

--- web_app_process.py
+++ web_app_process.py
@@ -2,41 +2,20 @@
 Main module for controlling Odisseus via web
 """
 import sys, getopt
+from multiprocessing import Process
 
 from flask import Flask
 from flask import render_template
 from flask import Response
 from flask import request
 
-# configuration parameters
-from odisseus_config import odisseus_config_obj
-
-HOST = odisseus_config_obj.HOST
-DEBUG = odisseus_config_obj.DEBUG
-PORT = odisseus_config_obj.PORT
-CONTROL_SERVER_INDEX_TEMPLATE_NAME =  odisseus_config_obj.CONTROL_SERVER_INDEX_TEMPLATE_NAME
-PROPULSION_CONTROL_TEMPLATE_NAME = odisseus_config_obj.PROPULSION_CONTROL_TEMPLATE_NAME
-
-IN_PIN_1_MOTOR_1 = odisseus_config_obj.IN_PIN_1_MOTOR_1
-IN_PIN_2_MOTOR_1 = odisseus_config_obj.IN_PIN_2_MOTOR_1
-ENA_MOTOR_1_PIN_ID = odisseus_config_obj.ENA_MOTOR_1_PIN_ID
-IN_PIN_1_MOTOR_2 = odisseus_config_obj.IN_PIN_1_MOTOR_2
-IN_PIN_2_MOTOR_2 = odisseus_config_obj.IN_PIN_2_MOTOR_2
-ENA_MOTOR_2_PIN_ID = odisseus_config_obj.ENA_MOTOR_2_PIN_ID
-
-ENABLE_LOG = odisseus_config_obj.ENABLE_LOG
-control_queue = odisseus_config_obj.control_queue
-
-from propulsion_process import PropulsionProcess
-from propulsion import PropulsionParams
-from propulsion import Propulsion
+from process_control_base import ProcessControlBase
+from control_cmds import TerminateProcessCMD
+from control_cmds import StartProcessCMD
 from control_cmds import PropulsionCmd
 
-
 app = Flask(__name__)
-
-# the control server used
-control_server = PropulsionProcess(odisseus_config=odisseus_config_obj)
+#web_app_process = None
 
 
 @app.route('/')
@@ -44,7 +23,12 @@ def index():
     """
     Function that serves the index view
     """
-    return render_template(CONTROL_SERVER_INDEX_TEMPLATE_NAME)
+    web_app_process = WebAppProcess.web_app_process
+
+    if web_app_process is None:
+        raise ValueError("WebApp process is None")
+
+    return render_template(web_app_process.get_config().CONTROL_SERVER_INDEX_TEMPLATE_NAME)
 
 
 """
@@ -65,23 +49,33 @@ def propulsion_control_view():
     """
     Returns the view for the propulsion control
     """
+    web_app_process = WebAppProcess.web_app_process
 
     if request.method == 'POST':
 
         # find out the direction and speed for creating the CMD
-        direction = request.form.get('direction')
-        speed = request.form.get('speed')
-        duration = request.form.get("duration")
+        speed = request.form.get('speed', None)
+        duration = request.form.get("duration", None)
+        direction = request.form.get('direction', None)
 
-        if ENABLE_LOG:
+        if direction == 'STOP' or direction is None or direction == "":
+            speed = 0
+            direction = 'STOP'
+
+        if duration is None or duration == "":
+            duration = 0
+            direction = 'STOP'
+            speed = 0
+
+        if web_app_process.get_config().ENABLE_LOG:
+
             print("direction: ", direction)
             print("speed: ", speed)
             print("duration: ", duration)
 
         prop_cmd = PropulsionCmd(direction=direction, speed_value=int(speed), duration=int(duration))
-        control_server.add_cmd(cmd=prop_cmd)
-
-    return render_template(PROPULSION_CONTROL_TEMPLATE_NAME)
+        web_app_process.add_propulsion_cmd(cmd=prop_cmd)
+    return render_template(web_app_process.get_config().PROPULSION_CONTROL_TEMPLATE_NAME)
 
 
 @app.route('/control/interrupt')
@@ -89,7 +83,8 @@ def interrupt_control():
     """
     Terminates the instance that Odisseus is running on
     """
-    control_server.terminate_odisseus_process()
+    web_app_process = WebAppProcess.web_app_process
+    web_app_process.terminate_odisseus_process()
     return index()
 
 
@@ -98,7 +93,8 @@ def start_control():
     """
     Recreates the Odisseus process
     """
-    control_server.spawn_odisseus_process()
+    web_app_process = WebAppProcess.web_app_process
+    web_app_process.spawn_odisseus_process()
     return index()
 
 
@@ -118,38 +114,76 @@ def control(control_name):
     return Response('Error...')
 
 
-def start_odisseus_web_app():
+class WebAppProcess(ProcessControlBase):
 
-    try:
+    """
+    The WebAppProcess allows for web control
+    """
 
-        opts, args = getopt.getopt(sys.argv, ["PLATFORM"])
+    web_app_process = None
 
-        if len(args) >= 2 and args[1].split('=')[1] == 'Ubuntu':
-            odisseus_config_obj.ON_RASP_PI = False
+    @staticmethod
+    def process_name():
+        return "WebApp"
 
-        # reset the mode
-        control_server.reset_mode()
+    """
+    @staticmethod
+    def set_process_instance(self, proc):
+        web_app_process = proc
+    """
 
-        prop_params = PropulsionParams(in_pin_1_motor_1=IN_PIN_1_MOTOR_1, in_pin_2_motor_1=IN_PIN_2_MOTOR_1, en_pin_motor_1=ENA_MOTOR_1_PIN_ID,
-                                       in_pin_1_motor_2=IN_PIN_1_MOTOR_2, in_pin_2_motor_2=IN_PIN_2_MOTOR_2, en_pin_motor_2=ENA_MOTOR_2_PIN_ID)
+    def __init__(self, odisseus_config, master_process):
+        ProcessControlBase.__init__(self, config=odisseus_config, name="WebApp")
+        self._master_process = master_process
 
-        propulsion = Propulsion(odisseus_config=odisseus_config_obj, params=prop_params)
-        #cmd_executor = CMDExecutor(cmd_queue=control_queue)
+    def start(self, **kwargs):
 
-        # initialize Odisseus
-        if ENABLE_LOG:
-            print("Initializing Odisseus...")
+        """
+        start the web server process
+        """
 
-        control_server.start(propulsion=propulsion) # cmd_executor=cmd_executor)
+        # is there a reason to spawn the process if it is active?
+        if self.is_alive():
+            if self.get_config().ENABLE_LOG:
+                print(self.get_name() + " process is alive nothing to do here...")
+            return
 
-        # finally start the web application
-        app.run(host=HOST, debug=DEBUG, port=PORT)
+        self.remove_interrupt()
+        WebAppProcess.web_app_process = self
+        self.set_process(proc=Process(target=app.run, kwargs={"host":self.get_config().HOST, "debug":self.get_config().DEBUG, "port":self.get_config().PORT}))
+        super(WebAppProcess, self).start(**kwargs)
 
-    finally:
 
-        control_server.terminate_odisseus_process()
-        control_server.cleanup_pins()
+    def terminate_odisseus_process(self):
+        self._master_process.add_cmd(cmd = TerminateProcessCMD(process_name="PropulsionProcess"))
+
+    def spawn_odisseus_process(self):
+        self._master_process.add_cmd(cmd=StartProcessCMD(process_name="PropulsionProcess"))
+
+    def add_propulsion_cmd(self, cmd):
+        self._master_process.add_cmd(cmd=cmd)
 
 
 if __name__ == '__main__':
-    start_odisseus_web_app()
+    from odisseus_config import odisseus_config_obj
+    from master_process import MasterProcess
+
+    opts, args = getopt.getopt(sys.argv, ["PLATFORM", ])
+
+    print(args)
+
+    if len(args) == 2:
+        PLATFORM = args[1].split('=')[1]
+
+        if PLATFORM == 'Ubuntu':
+            odisseus_config_obj.ON_RASP_PI = False
+
+    master_process = MasterProcess(odisseus_configuration=odisseus_config_obj)
+    master_process.start_process(proc_name=WebAppProcess.process_name())
+    web_app_process = master_process.get_process("WebApp")
+
+    if web_app_process is None:
+        raise ValueError("Could not create web_app_process")
+
+    master_process.run()
+
