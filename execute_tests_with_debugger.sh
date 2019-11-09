9be107ff12cda5fc1ccf70d52594ddb48050798b
David Hilbert <a.giavaras@gmail.com>
2019-11-09 19:19:40 +0200

    #58 add scripts to easy start tests in debug mode

---
+++ execute_tests_with_debugger.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+clear
+# Try to identify whether we are on Raspberry Pi or not
+# source the /etc/os-release values
+# This may not be the best option
+. /etc/os-release
+echo "System name $NAME"
+echo "Select the tests you want to run:
+
+1. Propulsion tests
+2. IR tests
+3. Ultrasound tests
+4. Camera tests
+5. Master process tests
+6. All tests
+0. Quit
+"
+
+read  -p "Enter option [0-6] > "
+
+if [[ "$REPLY" =~ ^[0-6]$ ]]; then
+	if [[ "$REPLY" == 0 ]]; then
+		echo "Terminating program"
+		exit
+	fi
+	if [[ "$REPLY" -eq 1 ]]; then
+		echo "Running Propulsion tests"
+		python3 -m pdb run_tests.py "PLATFORM=$NAME" "ID=$REPLY"
+		exit 0
+	fi
+	if [[ "$REPLY" -eq 2 ]]; then
+		echo "Running IR tests..."
+		echo "This is not enabled yet"
+		python3 -m pdb run_tests.py PLATFORM=$NAME ID=$REPLY
+		exit 0
+	fi
+	if [[ "$REPLY" -eq 3 ]]; then
+		echo "Ultrasound tests..."
+		python3 -m pdb run_tests.py PLATFORM=$NAME ID=$REPLY
+		exit 0
+	fi
+	if [[ "$REPLY" -eq 4 ]]; then
+		echo "Camera tests..."
+		python3 -m pdb run_tests.py PLATFORM=$NAME ID=$REPLY
+		exit 0
+	fi
+	if [[ "$REPLY" -eq 5 ]]; then
+	  echo "Master process tests..."
+	  python3 -m pdb run_tests.py PLATFORM=$NAME ID=$REPLY
+	  exit
+	fi
+	if [[ "$REPLY" -eq 6 ]]; then
+		echo "Running all tests..."
+		python3 -m pdb run_tests.py PLATFORM=$NAME ID=$REPLY
+		exit 0
+	fi
+else
+	echo "Unknown test type ${REPLY} was selected" >&2
+	exit 1
+fi
