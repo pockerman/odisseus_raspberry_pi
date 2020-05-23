55e4754a91737e58fea5823d7004ab3af04ee915
Alexandros Giavaras <a.giavaras@gmail.com>
2020-05-23 17:30:02 +0100

    #71 Correct value function

--- motion_model.py
+++ motion_model.py
@@ -4,29 +4,43 @@ import numpy as np
 class MotionModel(object):
 
 
-    def value(self, xk, u, v):
+    def __init__(self, Dt, tol):
+      self._state = np.array([0.0, 0.0, 0.0])
+      self._tol = tol
+
+
+    def value(self, u, werr):
         """
         Computes Odisseus next state dynamics
         The model adopted is a simple kinematic model for
         a differential drive system
 
-        :param xk: array of length 3 storing the previous state
-        :type  xk: array
-
         :param u: control input if length 2
         :type  u: array
 
-        :param v: array of length 2 represents unpredictable process noise
-        :type  v: array
+        :param werr: array of length 2 represents unpredictable process noise
+        :type  werr: array
 
         :return: np.array
         """
 
-        x_next = xk[0] + (u[0] + v[0]) * np.cos(xk[2] + u[1] + v[1])
-        y_next = xk[1] + (u[0] + v[0]) * np.sin(xk[2] + u[1] + v[1])
-        psi_next = xk[2] + u[1] + v[1]
+        # the angular velocity
+        w = u[1]
+        theta = self._state[2]
 
-        return np.array([x_next, y_next, psi_next])
+        if np.fabs(w) < self._tol:
+          # the orientation has not chenged
+
+          self._state[0] += (u[0]*self._Dt + werr[0])*np.cos(theta +  werr[1])
+          self._state[1] += (u[0]*self._Dt + werr[0])*np.sin(theta +  werr[1])
+        else:
+
+          self._state[2] += w*self._Dt + werr[1]
+          self._state[0] += (u[0]/(2*w) + werr[0])*(np.sin(self._state[2]) - np.sin(theta))
+          self._state[1] -= (u[0]/(2*w) + werr[0])*(np.cos(self._state[2]) - np.cos(theta))
+
+
+        return self._state
 
     def state_jacobian_matrix(self, xk, u, v):
         """
